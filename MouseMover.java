02d9b09c1598926c14ed466e8b787a3f67fdd374
rsalus <51675276+rsalus@users.noreply.github.com>
2019-06-10 13:44:48 -0400

    Create MouseMover.java
    simple anti-idle program

---
+++ MouseMover.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.*;
+import javax.swing.JFrame;
+
+public class MouseMover extends JFrame implements MouseListener
+{
+	public MouseMover() throws AWTException{
+		new JFrame("MouseMover");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setPreferredSize(new Dimension(150, 150));
+        this.addMouseListener(this);
+        this.pack();
+        this.setVisible(true);
+        
+        Robot mousie = new Robot();
+		Random random = new Random();
+		mousie.mouseMove(50, 50);
+		while(true){
+			mousie.delay(1000 * 10);
+			mousie.mouseMove(random.nextInt(120 + 1 - 20) + 20, random.nextInt(120 + 1 - 20) + 30);
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		new MouseMover();
+	}
+	public void mouseClicked(MouseEvent arg0){}
+	public void mouseEntered(MouseEvent arg0){}
+	public void mouseExited(MouseEvent arg0) {System.exit(0);}
+	public void mousePressed(MouseEvent arg0) {}
+	public void mouseReleased(MouseEvent arg0){}
+}
