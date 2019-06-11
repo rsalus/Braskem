e1ca63d1a4505c48f8501bb9687b8969a774ff5c
rsalus <51675276+rsalus@users.noreply.github.com>
2019-06-11 11:01:38 -0400

    Create MouseMover.java

---
+++ MouseMover.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+
+public class MouseMover extends JFrame implements MouseListener
+{
+	public MouseMover() throws AWTException{
+		new JFrame("MouseMover");
+		this.setPreferredSize(new Dimension(4, 4));
+		this.addMouseListener(this);
+		this.setUndecorated(true);
+		this.pack();
+		this.setVisible(true);
+
+		Robot mousie = new Robot();
+		mousie.mouseMove(2, 2);
+		int modifier = 1;
+		while(true){
+			mousie.delay(50);
+			mousie.mouseMove(MouseInfo.getPointerInfo().getLocation().x + modifier, MouseInfo.getPointerInfo().getLocation().y + modifier);
+			modifier = modifier*-1;
+		}
+	}
+	public static void main(String[] args) throws AWTException{
+		new MouseMover();
+	}
+	public void mouseClicked(MouseEvent arg0){}
+	public void mouseEntered(MouseEvent arg0){}
+	public void mouseExited(MouseEvent arg0) {System.exit(0);}
+	public void mousePressed(MouseEvent arg0) {}
+	public void mouseReleased(MouseEvent arg0){}
+}
